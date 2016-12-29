c797cf5c0e67e043561ac5fd48f741f789020e64
Ivo Georgiev <ivo@linvo.org>
2016-12-29 20:14:50 +0000

    works

---
+++ index.js
@@ -0,0 +1,28 @@
+var stream = require('stream')
+var util = require('util')
+
+var Transform = stream.Transform
+
+function Skip(options) {
+  // allow use without new
+  if (!(this instanceof Skip)) {
+    return new Skip(options);
+  }
+
+  this._toSkip = options.skip
+
+  // init Transform
+  Transform.call(this, options);
+}
+util.inherits(Skip, Transform);
+
+Skip.prototype._transform = function (chunk, enc, cb) {
+	if (this._toSkip == 0) this.push(chunk)
+	else if (this._toSkip > chunk.length) {
+		this._toSkip -= chunk.length;
+	} else {
+		this.push(chunk.slice(this._toSkip))
+		this._toSkip = 0;
+	}
+	cb();
+};
